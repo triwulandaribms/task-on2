2617bd882c0acc834a7d2c7a640a439a9ae8c963
Nsyamln <maulinainasnasyaa@gmail.com>
2024-07-05 19:27:02 +0700

    resetpassword

--- src/main/java/jawa/sinaukoding/sk/SkApplication.java
+++ src/main/java/jawa/sinaukoding/sk/SkApplication.java
@@ -1,24 +1,15 @@
 package jawa.sinaukoding.sk;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.sqlite.SQLiteDataSource;
+
 @SpringBootApplication
 @Configuration
 public class SkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkApplication.class, args);
-	}
-
-	@Bean
-	public SQLiteDataSource dataSource() {
-		SQLiteDataSource dataSource = new SQLiteDataSource();
-		dataSource.setUrl("jdbc:sqlite:D:/Sinau Koding/sksk/tools/db/data/sk.db");
-		// Set the busy timeout to 5000 milliseconds (5 seconds)
-		dataSource.setBusyTimeout(5000);
-		return dataSource;
 	}
 
 }
--- src/main/java/jawa/sinaukoding/sk/service/UserService.java
+++ src/main/java/jawa/sinaukoding/sk/service/UserService.java
@@ -1,6 +1,5 @@
 package jawa.sinaukoding.sk.service;
 
-import ch.qos.logback.core.net.SyslogOutputStream;
 import jawa.sinaukoding.sk.entity.User;
 import jawa.sinaukoding.sk.model.Authentication;
 import jawa.sinaukoding.sk.model.request.LoginReq;
@@ -27,7 +26,8 @@ public final class UserService extends AbstractService {
     private final PasswordEncoder passwordEncoder;
     private final byte[] jwtKey;
 
-    public UserService(final Environment env, final UserRepository userRepository, final PasswordEncoder passwordEncoder) {
+    public UserService(final Environment env, final UserRepository userRepository,
+            final PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         final String skJwtKey = env.getProperty("sk.jwt.key");
@@ -40,7 +40,7 @@ public final class UserService extends AbstractService {
                 return Response.badRequest();
             }
             final List<UserDto> users = userRepository.listUsers(page, size) //
-                    .stream().map(user -> new UserDto(user.name(),user.role())).toList();
+                    .stream().map(user -> new UserDto(user.name(), user.role())).toList();
             return Response.create("09", "00", "Sukses", users);
         });
     }
@@ -112,7 +112,7 @@ public final class UserService extends AbstractService {
             return Response.create("08", "02", "password salah", null);
         }
         final Authentication authentication = new Authentication(user.id(), user.role(), true);
-        System.out.println("auth : "+authentication);
+        System.out.println("auth : " + authentication);
         final long iat = System.currentTimeMillis();
         final long exp = 1000 * 60 * 60 * 24; // 24 hour
         final JwtUtils.Header header = new JwtUtils.Header() //
@@ -127,9 +127,8 @@ public final class UserService extends AbstractService {
         return Response.create("08", "00", "Sukses", token);
     }
 
-
-    public Response<Object> resetPassword(final Authentication authentication, final ResetPasswordReq req){
-        System.out.println("Authh : "+authentication);
+    public Response<Object> resetPassword(final Authentication authentication, final ResetPasswordReq req) {
+        System.out.println("Authh : " + authentication);
 
         if (req.newPassword() == null || req.newPassword().isEmpty()) {
             return Response.create("07", "03", "New password must not be empty", null);
@@ -145,11 +144,12 @@ public final class UserService extends AbstractService {
             }
 
             User user = userOpt.get();
-            if (passwordEncoder.matches(req.newPassword(),user.password())){
-                return Response.create("07", "03", "Old password and new password are same. Please use a new password", null);
+            if (passwordEncoder.matches(req.newPassword(), user.password())) {
+                return Response.create("07", "03", "Old password and new password are same. Please use a new password",
+                        null);
             }
 
-            if (passwordEncoder.matches(req.newPassword(),user.password())){
+            if (passwordEncoder.matches(req.newPassword(), user.password())) {
                 return Response.create("07", "03", "was Deleted", null);
             }
             String newEncodedPassword = passwordEncoder.encode(req.newPassword());
@@ -157,10 +157,10 @@ public final class UserService extends AbstractService {
             if (updatedUserId == 0L) {
                 return Response.create("07", "02", "Failed to update password", null);
             }
-            if (user.deletedBy() != null || user.deletedAt()!=null) {
+            if (user.deletedBy() != null || user.deletedAt() != null) {
                 return Response.create("07", "04", "User account has been deleted", null);
             }
-            UserDto userDto = new UserDto(user.name(),user.role());
+            UserDto userDto = new UserDto(user.name(), user.role());
             return Response.create("07", "00", "Success to update password", userDto);
         } catch (Exception e) {
             return Response.create("07", "02", "Invalid Token", null);
